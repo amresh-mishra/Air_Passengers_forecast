173fe347cf609f42e58fd89907361fc248144d7d
AMRESH  MISHRA <121451346+amresh-mishra@users.noreply.github.com>
2023-04-16 21:34:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # Import necessary packages
 import streamlit as st
 import pickle
-import pandas as pd
+ 
 import numpy as np
-import matplotlib.pyplot as plt
+ 
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 model=pickle.load(open('pipe.pkl','rb'))
 df=pickle.load(open('df.pkl','rb'))
